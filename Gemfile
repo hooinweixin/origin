c5e6e549a52885cf55130f0972fe0ff549187e82
xuranci <635583102@qq.com>
2014-08-25 17:30:25 +0800

    add

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://ruby.taobao.org/'
+
+gem 'rails', '3.2.8'
+#gem 'sqlite3'
+gem 'mysql2'
+
+group :assets do
+end
+
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'jbuilder'
+gem 'unicorn'
+
+gem 'rest-client'
+gem 'faraday'
+gem 'qiniu-rs'
+# gem 'capistrano'
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weixin::Application.config.secret_token = 'a3172a41e26ce27e7537ecd6225b46a6add2cf3e5d2659a6cb768eb22c3e640dc4ac3e198cd82e972d8c1ef9f750c62ce0a999122347031e5a11548d5d9ad713'
