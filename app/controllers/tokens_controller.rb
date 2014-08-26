086ec1d9b8d035e790cbc6b2af9d871d9f3177b3
think <think@ubuntu.ubuntu-domain>
2014-08-26 17:54:46 +0800

    add

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,11 +1,23 @@
 class TokensController < ApplicationController
 
+require 'digest/sha1'
 
 	def get
+		sign = params[ :signature]
+		time = params[ :timestamp]
+		nonce = params[ :nonce]
 
+		token = "huyindianzishangwu"
 
+		array = [token , time , nonce]
 
-		return false;
+		array = array.sort 
+
+		str = array[0] + array[1] + array[2]
+
+		str = Digest::SHA1.hexdigest(str)
+
+		render(:text => str)   
 	end
 
 
